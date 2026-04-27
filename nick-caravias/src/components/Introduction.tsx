7381f5a8c1bc58ec4ee49d8737ec95e896b38515
NickCaravias <nick.caravias@gmail.com>
2026-04-27 12:21:10 -0400

    update skills

--- nick-caravias/src/components/Introduction.tsx
+++ nick-caravias/src/components/Introduction.tsx
@@ -3,7 +3,7 @@ function Introduction() {
     <>
       <div className="flex items-center justify-center min-h-[50vh] mb-8">
         <p className="text-2xl text-center max-w-3xl"> 
-          Hello! I am a full-stack software engineer passionate about creating elegant solutions with code. 
+          Hello! I am a software engineer passionate about creating elegant solutions with code. 
         </p>
       </div>
     </>
